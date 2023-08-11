503eb30fc8164eb89c3f4b0b8710c2a215d8de02
Iris Lai <s3799602@rmit.edu.vn>
2023-08-11 09:07:46 +0700

    Initial Commit

---
+++ simple dice game/simple_dice_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_dice_gameApp.swift
+//  simple dice game
+//
+//  Created by Trí Lai on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct simple_dice_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
